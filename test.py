a95338fdc86eec1c1d51ff56efc3c196574a3aff
abm-tosh <adrian.mackenzie@anu.edu.au>
2019-02-13 22:28:51 +1100

    getting auth working

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
 import praw
+import pandas as pd
+
+# read the authorisation info
+
+auth = open('secret.txt') 
+lines_r = auth.readlines()
+lines = [line.rstrip('\n') for line in lines_r]
+print lines
+
+
+reddit = praw.Reddit(client_id=lines[0], \
+                     client_secret=lines[1], \
+                     user_agent=lines[2], \
+                     username=lines[3], \
+                     password=lines[4])
+
+subreddit = reddit.subreddit('Nootropics')
+
+top_subreddit = subreddit.top()
+
+
+for submission in subreddit.top(limit=1):
+    print(submission.title, submission.id)
